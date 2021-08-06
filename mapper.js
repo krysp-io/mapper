b5e9666280abcdd175afbec7588d6ad563489106
Eugene Zaidun <vimoj2@gmail.com>
2021-08-06 12:46:53 +0200

    first commit

---
+++ mapper.js
@@ -0,0 +1,30 @@
+const lodash = require('lodash');
+
+module.exports = function (RED) {
+  function MappingNode(n) {
+    RED.nodes.createNode(this, n);
+    this.in = n.in;
+    this.out = n.out;
+    this.url = n.url;
+  }
+
+  RED.nodes.registerType("mapping", MappingNode);
+
+  function MapperNode(n) {
+    RED.nodes.createNode(this, n);
+    this.mapping = RED.nodes.getNode(n.mapping);
+
+    var node = this;
+    this.rules = n.rules;
+    node.on("input", function (msg) {
+      for(let i = 0; i < this.rules.length; i++) {
+        const rule = this.rules[i];
+        lodash.set(msg.payload, rule.po, lodash.get(msg.payload, rule.pi)); 
+      }
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType("mapper", MapperNode);
+};
+  
